b451b0b1c1d2f255e17b515008323a71d06f5eff
Isuru Liyanage <induwara44@gmail.com>
2020-11-01 14:57:29 +0800

    test project

---
+++ testisuru/src/main/java/com/test/testisuru/TestIsuruApplication.java
@@ -0,0 +1,11 @@
+package com.test.testisuru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestIsuruApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TestIsuruApplication.class, args);
+	}
+}
